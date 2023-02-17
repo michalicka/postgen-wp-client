5034cc156b781220a9a404de3eda831792554c13
michalicka <git@jimi.sk>
2023-02-17 23:30:22 +0100

    format

--- api.php
+++ api.php
@@ -6,18 +6,18 @@ include_once('./functions.php');
 header('Content-Type: application/json');
 
 if (!get_option('pgwc_enabled')) {
-  http_response_code(401);
-  echo json_encode(['error' => 'Disabled']);
-  exit();
+    http_response_code(401);
+    echo json_encode(['error' => 'Disabled']);
+    exit();
 }
 
 if (!isset($_SERVER['HTTP_AUTHORIZATION']) 
-  || empty($_SERVER['HTTP_AUTHORIZATION']) 
-  || $_SERVER['HTTP_AUTHORIZATION'] !== 'Bearer '.get_option('pgwc_api_key')
+    || empty($_SERVER['HTTP_AUTHORIZATION']) 
+    || $_SERVER['HTTP_AUTHORIZATION'] !== 'Bearer '.get_option('pgwc_api_key')
 ) {
-  http_response_code(401);
-  echo json_encode(['error' => 'Unauthorized']);
-  exit();
+    http_response_code(401);
+    echo json_encode(['error' => 'Unauthorized']);
+    exit();
 }
 
 $request_body = file_get_contents('php://input');
@@ -25,17 +25,15 @@ $data = json_decode($request_body);
 $post = $data->post;
 
 $wpPost = [
- 'post_title' => $post->title ?? '',
- 'post_content' => $post->content ?? '',
- 'post_excerpt' => $post->excerpt ?? '',
- 'post_date' => $post->date ?? '',
- 'post_status' => ($post->date ?? '') && strtotime($post->date) > time() ? 'future' : get_option('pgwc_status', 'publish'),
- 'post_author' => get_option('pgwc_author_id'),
- 'post_category' => [ get_cat_ID($post->category ?? '') ?: get_option('pgwc_category_id') ],
- 'tags_input' => $post->tags ?? [],
+    'post_title' => $post->title ?? '',
+    'post_content' => $post->content ?? '',
+    'post_excerpt' => $post->excerpt ?? '',
+    'post_date' => $post->date ?? '',
+    'post_status' => ($post->date ?? '') && strtotime($post->date) > time() ? 'future' : get_option('pgwc_status', 'publish'),
+    'post_author' => get_option('pgwc_author_id'),
+    'post_category' => [ get_cat_ID($post->category ?? '') ?: get_option('pgwc_category_id') ],
+    'tags_input' => $post->tags ?? [],
 ];
-
-// echo json_encode($wpPost); die;
 
 if ($data->action === 'create') {
     $postId = wp_insert_post($wpPost);
